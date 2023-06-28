c9b99454aa47c4a6ee5574c6c4e539bba83586f6
rahul singaria <singariarahul345@gmail.com>
2023-06-28 18:06:09 +0530

    firebase config

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -47,7 +47,7 @@ class _HomeState extends State<Home> {
                               style: TextStyle(
                                 fontWeight: FontWeight.w600,
                                 fontSize: 19,
-                                color: Colors.black,
+                                color: Colors.red,
                               ),
                             ),
                             Text(
--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,19 @@ import 'package:managment/widgets/bottomnavigationbar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/widgets.dart';
 import 'data/model/add_date.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// void main() async {
+//   await Hive.initFlutter();
+//   Hive.registerAdapter(AdddataAdapter());
+//   await Hive.openBox<Add_data>('data');
+//   runApp(const MyApp());
+// }
 
 void main() async {
-  await Hive.initFlutter();
-  Hive.registerAdapter(AdddataAdapter());
-  await Hive.openBox<Add_data>('data');
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
